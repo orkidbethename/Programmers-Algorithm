fb96cd657c792c6e29019ebbc6ea1f1cdce2372f
Nanhee Kim <82951359+orkidbethename@users.noreply.github.com>
2023-04-23 00:23:57 -0400

    Update 달리기 경주.java

--- 달리기 경주.java
+++ 달리기 경주.java
@@ -31,3 +31,36 @@ class Solution {
     }
 }
 
+//시간 단축
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] players, String[] callings) {
+        String[] answer = new String[players.length];
+        Map<Integer,String> nameMap = new TreeMap<>();
+        Map<String,Integer> rankMap = new HashMap<>();
+        
+        for(int i=0; i<players.length; i++){
+            nameMap.put(i+1, players[i]);
+            rankMap.put(players[i],i+1);
+        }
+        
+        for(int i=0; i<callings.length; i++){
+            String call = callings[i];
+            int index = rankMap.get(call);
+            String loose = nameMap.get(index-1);
+            
+            rankMap.put(call,index-1);
+            rankMap.put(loose,index);
+            nameMap.put(index-1,call);
+            nameMap.put(index,loose);
+        }
+        
+        int index = 0;
+        for(int key : nameMap.keySet()){
+            answer[index++] = nameMap.get(key);
+        }
+        
+        return answer;
+    }
+}
