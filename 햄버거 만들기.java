bec70864cd187062548c1f100d49400bdf4737cd
Nanhee Kim <82951359+orkidbethename@users.noreply.github.com>
2023-04-23 01:04:45 -0400

    Update 햄버거 만들기.java

--- 햄버거 만들기.java
+++ 햄버거 만들기.java
@@ -15,3 +15,36 @@ class Solution {
         return answer;
     }
 }
+
+//시간 단축
+import java.util.*;
+import java.util.stream.*;
+
+class Solution {
+    public int solution(int[] ingredient) {
+        int answer = 0;
+        
+        ArrayList<Integer> list = new ArrayList<>(
+            Arrays.stream(ingredient).boxed().collect(Collectors.toList()));
+        
+        for(int i=0; i<list.size() && list.size()>3; i++){
+            if(list.get(i) == 1){
+                if(i>2){
+                    if(list.get(i-1) == 3 && list.get(i-2) == 2 && list.get(i-3) == 1){
+                        list.remove(i);
+                        list.remove(i-1);
+                        list.remove(i-2);
+                        list.remove(i-3);
+                        
+                        answer++;
+                        
+                        i -= 4;
+                        continue;
+                    }
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
