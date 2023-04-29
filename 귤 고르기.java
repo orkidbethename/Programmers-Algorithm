2fb37f17553bd46d691111f5a529382faeb24d0e
Nanhee Kim <82951359+orkidbethename@users.noreply.github.com>
2023-04-29 13:31:03 -0400

    Update 귤 고르기.java

--- 귤 고르기.java
+++ 귤 고르기.java
@@ -31,3 +31,30 @@ class Solution {
         return answer;
     }
 }
+
+// 시간 단축
+import java.util.*;
+
+class Solution {
+    public int solution(int k, int[] tangerine) {
+        int answer = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for(int tan:tangerine){
+            map.put(tan, map.getOrDefault(tan,0)+1);
+        }
+        
+        List<Integer> list = new ArrayList<>(map.values());
+        Collections.sort(list, Collections.reverseOrder());
+        
+        for(int a:list){
+            k -= a;
+            answer++;
+            if(k<1){
+                return answer;
+            }
+        }
+        
+        return answer;
+    }
+}
