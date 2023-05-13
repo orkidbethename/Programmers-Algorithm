9d8f319372083ae1a759a897bb1d7b822364d06e
Nanhee Kim <82951359+orkidbethename@users.noreply.github.com>
2023-05-13 19:02:24 -0400

    Update n^2 배열 자르기.java

--- n^2 배열 자르기.java
+++ n^2 배열 자르기.java
@@ -28,3 +28,20 @@ class Solution {
         return answer;
     }
 }
+
+//메모리 초과 수정
+class Solution {
+    public int[] solution(int n, long left, long right) {
+        int[] answer = new int[(int)(right-left)+1];
+        
+        for(int i=0; i<answer.length; i++){
+            int x = (int)(left/n+1);
+            int y = (int)(left%n+1);
+            
+            left++;
+            answer[i] = Math.max(x,y);
+        }
+        
+        return answer;
+    }
+}
