32c6b7a40f2087406f6a8b7f64bc3581aa2fbc93
Nanhee Kim <82951359+orkidbethename@users.noreply.github.com>
2023-08-07 15:14:31 +0900

    Update 둘만의 암호.java

--- 둘만의 암호.java
+++ 둘만의 암호.java
@@ -1,38 +1,18 @@
-import java.util.*;
-
 class Solution {
     public String solution(String s, String skip, int index) {
         String answer = "";
-        int[] skipArr = new int[skip.length()];
-        int[] result = new int[s.length()];
+        int idx;
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
         
         for(int i=0; i<skip.length(); i++){
-            skipArr[i] = (int)skip.charAt(i);
+            alphabet = alphabet.replace(Character.toString(skip.charAt(i)),"");
         }
         
         for(int i=0; i<s.length(); i++){
-            int ascii = (int)s.charAt(i);
-            
-            int real = ascii + index;
-            result[i] = real;
-            
-            for(int j=0;j<skipArr.length; j++){
-                if(result[i]>=skipArr[j] && result[i]-index<skipArr[j] ){
-                    result[i]++;
-                    if(result[i]>122){
-                        result[i] = result[i]-123+97;
-                    }
-                    System.out.println(ascii);
-                }
-                
-            }
-            
-            answer += (char)result[i];
+            idx = alphabet.indexOf(Character.toString(s.charAt(i))) + index;
+            answer += Character.toString(alphabet.charAt(idx%alphabet.length()));
         }
         
-        System.out.println(answer);
-        
-
         return answer;
     }
 }
